5fdee015d0bdfddd26163228a85e8dac8ce180ae
minh <minhtv.work@gmail.com>
2024-03-25 10:28:10 +0700

    Dto and Helper

---
+++ src/aspnet-core/PTS.Base.Application/Dto/PagedInputDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Abp.Application.Services.Dto;
+
+namespace PTS.Base.Application.Dto
+{
+    public class PagedInputDto : IPagedResultRequest
+    {
+        [Range(1, 999999)]
+        public int MaxResultCount { get; set; } = 999999;
+
+        [Range(0, int.MaxValue)]
+        public int SkipCount { get; set; } = 0;
+
+        public PagedInputDto()
+        {
+            MaxResultCount = 10;
+        }
+    }
+}
---
+++ src/aspnet-core/PTS.Base.Application/Dto/PagedSortedAndFilteredInputDto.cs
@@ -0,0 +1,7 @@
+﻿namespace PTS.Base.Application.Dto
+{
+    public class PagedSortedAndFilteredInputDto : PagedAndSortedInputDto
+    {
+        public string? Filter { get; set; }
+    }
+}
