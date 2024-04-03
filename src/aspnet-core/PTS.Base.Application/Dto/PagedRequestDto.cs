121654682410536c87f9ffbd01a089ed6373116e
minh <minhtv.work@gmail.com>
2024-04-03 23:59:48 +0700

    PagedRequest

---
+++ src/aspnet-core/PTS.Base.Application/Dto/PagedRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTS.Base.Application.Dto
+{
+    public class PagedRequestDto
+    {
+        public int SkipCount { get; set; }
+        public int MaxResultCount { get; set; }
+    }
+}
